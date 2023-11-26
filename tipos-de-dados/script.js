5fb0df3c225dab9107d7dd4326d957f98cb5504b
Xavvir <leandroxm1997@hotmail.com>
2023-11-26 20:23:05 -0300

    atualização1

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,8 @@
+var nome = 'Leandro'
+var idade = 26
+var frase = "It's time"
+
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos');
+
+console.log(typeof nome, typeof idade);
+console.log(frase);
---
+++ variaveis/script.js
@@ -0,0 +1,7 @@
+var nome = 'Leandro';
+var idade = 26;
+var comida
+comida = 'sushi';
+
+
+console.log(nome, idade, comida);
